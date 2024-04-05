f1e2536157e45394e3bd9f4b879c4a89c805feb4
Ebredvick <Embredvick@gmail.com>
2024-04-05 09:41:08 +0200

    add update config method for download_url

--- gitbash/setup.bat
+++ gitbash/setup.bat
@@ -20,7 +20,7 @@ if exist %INSTALLER% (
     goto :Install
 ) else (
     echo Installer does not exist
-    goto :Download
+    goto :SetupDownload
 )
 
 :DeleteDownload
@@ -28,21 +28,34 @@ echo ~~~~~ Deleting %INSTALLER% ~~~~~
 del %INSTALLER%
 goto :Download
 
-:Download
+:SetupDownload
 echo Current URL is %DOWNLOAD_URL%
 echo 1. Use Current URL
 echo 2. Enter New URL
 set /p choice="Enter your choice: "
 if !choice!==2 (
     set /p DOWNLOAD_URL="Enter the new URL: "
-    ::Replace the Old URL with the New URL in config.txt
-    set "DOWNLOAD_URL=!DOWNLOAD_URL:/=\/!"
+
+    set search=DOWNLOAD_URL
+    set replace=DOWNLOAD_URL="!DOWNLOAD_URL!"
+
+    for /f "tokens=1* delims==" %%a in (config.txt) do (
+        if %%a==!search! (
+            echo !replace! >> temp.tmp
+        ) else (
+            echo %%a=%%b >> temp.tmp
+        )
+    )
+    move /y temp.tmp config.txt
 )
+
+
+:Download
 echo ~~~~~ Downloading %TOOL_NAME% ~~~~~
 curl -L -o %INSTALLER% %DOWNLOAD_URL%
 timeout /t 3 /nobreak > nul
 
-:: Exit directory of this script
+Exit directory of this script
 cd ..
 
 ENDLOCAL
