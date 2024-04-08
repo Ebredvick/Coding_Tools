b5946c126ee98566bd9d9f9efaca9558b167d0a9
Ebredvick <Embredvick@gmail.com>
2024-04-08 12:41:35 +0200

    Fixed null reference, echo line not in proper location

--- anaconda/install.bat
+++ anaconda/install.bat
@@ -1,14 +1,14 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 
-echo ~~~~~ Installing %TOOL_NAME% ~~~~~
-
 cd ./anaconda
 
 :: Read the config file
 for /f "tokens=1* delims==" %%a in (config.txt) do (
     call set %%a=%%b
 )
+
+echo ~~~~~ Running %TOOL_NAME% Uninstaller ~~~~~
 
 :Install
 %INSTALLER% /InstallationType=JustMe /RegisterPython=0 /S /D=%INSTALL_LOCATION%
