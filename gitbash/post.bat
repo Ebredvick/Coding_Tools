99d94e3482d60f921b00111f9fbec8862f0ae6c2
Ebredvick <Embredvick@gmail.com>
2024-04-08 11:51:03 +0200

    fixed post config implementation for gitbash

--- gitbash/post.bat
+++ gitbash/post.bat
@@ -9,34 +9,31 @@ for /f "tokens=1* delims==" %%a in (config.txt) do (
 )
 
 echo ~~~~~ Configuring Git ~~~~~
-set "bashrc=%USERPROFILE%\.bashrc"
-set "gitconfig=%USERPROFILE%\.gitconfig"
-echo # .bashrc > "%bashrc%"
-echo # .gitconfig > "%gitconfig%"
+:: Set Home Directory
+::echo export HOME="%UserProfile%/dev" >> %UserProfile%\Git\etc\profile
 
 :: Set User Name and Email
 set /p GIT_NAME="Enter Your Git User Name: "
 set /p GIT_EMAIL="Enter Your Git Email: "
-echo [user] > "%gitconfig%"
-echo     name = %GIT_NAME% >> "%gitconfig%"
-echo     email = %GIT_EMAIL% >> "%gitconfig%"
 
-::Set Alias
-echo [alias] >> "%gitconfig%"
-echo     "lg = log --color --graph --pretty=format:'%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset' --abbrev-commit" >> "%gitconfig%"
-
-::Set nano as default editor
-echo [core] >> "%gitconfig%"
-echo     editor = nano >> "%gitconfig%"
+!GIT_BIN_PATH! config --global user.name "%GIT_NAME%"
+!GIT_BIN_PATH! config --global user.email "%GIT_EMAIL%"
+!GIT_BIN_PATH! config --global alias.lg "log --color --graph --pretty=format:'%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset' --abbrev-commit"
+!GIT_BIN_PATH! config --global core.editor "nano"
 
 ::Add Conda command to Git Bash
+if exist %UserProfile%\.bashrc (
+    del %UserProfile%\.bashrc
+)
 echo . /c/Users/%USERNAME%/anaconda3/etc/profile.d/conda.sh >> %USERPROFILE%/.bashrc
 echo export PATH="%USERPROFILE%/anaconda3:$PATH" >> %USERPROFILE%/.bashrc
 
-:: Set Home Directory
-echo export HOME="%UserProfile%/dev" >> %UserProfile%\Git\etc\profile
+::Create .bash_profile
+if exist %UserProfile%\.bash_profile (
+    del %UserProfile%\.bash_profile
+)
+echo source ~/.bashrc >> %UserProfile%\.bash_profile
 
 :: Create Shortcut On Desktop
 powershell -Command "$WScriptShell = New-Object -ComObject WScript.Shell; $Shortcut = $WScriptShell.CreateShortcut('"%SHORTCUT_PATH%"\Git Bash.lnk'); $Shortcut.TargetPath = '!GIT_BASH_BIN_PATH!'; $Shortcut.Save()"
-pause
 cd ..
