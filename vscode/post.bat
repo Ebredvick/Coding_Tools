5242e7984b7db9f74787e021402a0c15943bee51
Ebredvick <Embredvick@gmail.com>
2024-04-10 17:07:29 +0200

    Add Vscode post registry key edits for Open With Code

--- vscode/post.bat
+++ vscode/post.bat
@@ -17,6 +17,50 @@ if not exist %APPDATA%/Code/User (
 
 xcopy .\settings.json %APPDATA%\Code\User /i /y
 
+::Registry Key to Open Files with VSCode
+SET KEY_NAME="HKEY_CLASSES_ROOT\Directory\shell\VSCode"
+SET VALUE_NAME=""
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="Open w&ith Code"
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
+SET KEY_NAME="HKEY_CLASSES_ROOT\Directory\shell\VSCode"
+SET VALUE_NAME="Icon"
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe"
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
+SET KEY_NAME="HKEY_CLASSES_ROOT\Directory\shell\VSCode\command"
+SET VALUE_NAME=""
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="\"%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe\" \"%%V\""
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
+::Registry Key to Open Folders with VSCode
+SET KEY_NAME="HKEY_CLASSES_ROOT\*\shell\VSCode"
+SET VALUE_NAME=""
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="Open w&ith Code"
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
+SET KEY_NAME="HKEY_CLASSES_ROOT\*\shell\VSCode"
+SET VALUE_NAME="Icon"
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe"
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
+SET KEY_NAME="HKEY_CLASSES_ROOT\*\shell\VSCode\command"
+SET VALUE_NAME=""
+SET VALUE_TYPE="REG_SZ"
+SET VALUE_DATA="\"%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe\" \"%%1\""
+
+REG ADD %KEY_NAME% /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA% /f
+
 powershell -Command "$WScriptShell = New-Object -ComObject WScript.Shell; $Shortcut = $WScriptShell.CreateShortcut('%SHORTCUT_PATH%\VS Code.lnk'); $Shortcut.TargetPath = '%VSCODE_PATH%'; $Shortcut.Save()"
 
 cd ..
